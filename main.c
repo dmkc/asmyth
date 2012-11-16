23fae3d5c420f744de200d7f98f952a07a9ed19b
Dmitry Kichenko <dmitrykichenko@gmail.com>
2012-11-16 16:20:12 -0500

    Keyboard handling API changes

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 struct Queue{
 	int position;
 	int length;
-	int sample_queue[10000];
+	int sample_queue[96000];
 };
 
 typedef struct Queue Queue;
@@ -15,7 +15,17 @@ int Queue_set_length(Queue*, int length);
 
 // Queue of samples to be pushed to AudioCodec's FIFO
 Queue pulse_queue;
-Queue saw_queue;
+Queue saw1_queue;
+Queue saw2_queue;
+
+// whether a key is currently pressed. Used by envelope.
+int keyPressed;
+// a flag set by keyboard interrupt handler to make sure the waveforms 
+// are regenerated
+int regenerateWave;
+// how much to multiply the base frequency by
+int frequencyOffset;
+int baseFrequency;
 
 void initialize();
 void playBuffer();
@@ -23,16 +33,33 @@ void createPulse(Queue*, int frequency, signed int amp);
 void createSaw(  Queue*, int frequency, signed int amp);
 int  debug();
 
+/**
+ * Where things begin.
+ */
 int main() {
-	createSaw(&saw_queue, 55, 90000000);
-	createPulse(&pulse_queue, 54, 90000000);
+	frequencyOffset = 0;
+	baseFrequency = 55;
 
+	createWaves();
 	initialize();
-	//pulse_queue[0] = 7;
-	
 
 	while(1){ 
-		//*ADDR_RLED = *ADDR_SLIDESWITCHES;
 		// loop forevaaa
 	}
+}
+
+/**
+ * Generate waveforms for all oscillators.
+ *
+ * Use baseFrequency + frequencyOffset as frequencies.
+ */
+void createWaves() {
+	createSaw(&saw1_queue, 
+			 baseFrequency + frequencyOffset, 90000000);
+	createSaw(&saw2_queue, 
+		 baseFrequency + frequencyOffset, 90000000);
+	createPulse(&pulse_queue, 
+			baseFrequency + frequencyOffset + 1, 90000000);
+	
+	regenerateWave = 0;
 }
